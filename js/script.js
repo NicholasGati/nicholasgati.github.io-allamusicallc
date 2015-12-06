c6ca74a630c20517e7c63000ba50c4a4e9f01059
NicholasGati <ngati016@hotmail.com>
2015-12-06 15:21:59 -0500

    added more pages and styled throughout site

--- js/script.js
+++ js/script.js
@@ -1,11 +1,45 @@
+var tableInfo = [["KILD TV", "2015", "Feature Film", "William Collins", "Score, Mix"],["Rena's Promise", "2015", "Audiobook", "Heather Dune Macadam", "Audiobook Recording"],["Keith Urban's '30 Songs in 30 Days'", "2014", "DVD Series", "Ingenious Designs (HSN)", "Solo/Duo Section: Edit, Mix"],["A Little Christmas Business", "2014", "Feature Film", "Chuck Walker", "Editing, Sound Design, Mix"],["The Rolling Road", "2014", "Short Film", "William Collins", "Score, Sound Design, Mix"],["The Amateur", "2014", "Feature Film", "Jacqueline Cross", "Score, ADR Recording"],["Sorrow", "2014", "Feature Film", "Millie Loredo", "Sound Design, Mix"],["Return to Vengeance", "2014", "Feature Film", "Chuck Walker", "Restoration, Sound Design, Mix"],["Serenade Duo", "2014", "CD", "Serenade Duo", "Audio Mastering"],["Me Gusta Como Baila", "2014", "Single", "Jay y Lexi", "Audio Mastering"],["Metatron & the archangels of the throne", "2014", "Rock Opera", "Kean Roger Risko", "Score (2 pieces)"],["Apex Rising", "2013", "Trailer", "James Terriaca", "Score"],["The Art of Giants", "2013", "Documentary", "William Collins", "Acquisition, Score, Sound Design, Mix"]];
+
+var tableHeader = ["Project Title", "Year", "Type", "Client/Director", "Service"];
+
 $(document).ready(function() {
 
 	// toggling the right side navbar
+	var isShowing = false;
 	$('#icon').click(function() {
-		$('#nav-area').toggle(function() {
-			$('#nav-area').addClass("display");
-			$(this).animate({left: '100%'}, 1000);
-		});
+		if(isShowing == true) {
+			$('#nav-area').hide(500, function() {
+				isShowing = false;
+			});
+		} else {
+			$('#nav-area').show(500, function() {
+				isShowing = true;
+			});	
+		}
+		
 	});
 
+	//displaying table data
+	var table = $('<table>').addClass('theTable');
+
+	for (var a = 0; a < tableHeader.length; a++) {
+		var head = $('<th>').addClass('theHead');
+		table.append(head);
+		head.html(tableHeader[a]);
+	}
+	
+	for(var i = 0; i < tableInfo.length; i++) {
+    for(var j = 0; j < tableInfo[i].length; j++) {
+    	var row = $('<tr>').addClass('theRow');
+    	var data = $('<td>').addClass('theData');
+    	data.html(tableInfo[i][j]);
+    	row.append(data);
+    	table.append(row);
+    }
+	}
+	$('#theTable').append(table);
+
 });
+
+
+
